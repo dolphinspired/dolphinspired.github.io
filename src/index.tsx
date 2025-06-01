fd4dcbe6da57d0877652cbcee186b9781f1d16e6
dolphinspired <ultima749@gmail.com>
2025-06-01 18:19:26 -0400

    Material UI hello world

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// meta tag is to add reponsive scaling for MUI
+// See: https://mui.com/material-ui/getting-started/usage/
 root.render(
   <React.StrictMode>
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <CssBaseline enableColorScheme />
     <App />
   </React.StrictMode>
 );
